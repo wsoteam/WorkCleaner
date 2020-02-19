af4731935d518ff86da5baf21df26d145410efce
Wadim <waadimka@mail.ru>
2020-02-19 17:17:17 +0300

    battery graph ui
    https://wsoteam.atlassian.net/browse/TN-19729

---
+++ app/src/main/java/cleaner/booster/wso/app/inapp/BatteryGraphAct.kt
@@ -0,0 +1,7 @@
+package cleaner.booster.wso.app.inapp
+
+import androidx.appcompat.app.AppCompatActivity
+import cleaner.booster.wso.app.R
+
+class BatteryGraphAct :AppCompatActivity(R.layout.battery_graph_act) {
+}
---
+++ app/src/main/java/cleaner/booster/wso/app/inapp/SecurityGraphAct.kt
@@ -0,0 +1,7 @@
+package cleaner.booster.wso.app.inapp
+
+import androidx.appcompat.app.AppCompatActivity
+import cleaner.booster.wso.app.R
+
+class SecurityGraphAct : AppCompatActivity(R.layout.security_graph_act) {
+}
