4fd71da25c69871e299c041a2a57dc6ac821e74e
Wadim <waadimka@mail.ru>
2020-02-20 11:21:36 +0300

    ui rocket
    https://wsoteam.atlassian.net/browse/TN-19722

---
+++ app/src/main/java/cleaner/booster/wso/app/inapp/RocketAct.kt
@@ -0,0 +1,7 @@
+package cleaner.booster.wso.app.inapp
+
+import androidx.appcompat.app.AppCompatActivity
+import cleaner.booster.wso.app.R
+
+class RocketAct : AppCompatActivity(R.layout.rocket_act) {
+}
